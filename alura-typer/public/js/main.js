4ea20f58d7108de7de9005bd024df2034618a9b7
Rodrigo Melo <romelo@Rodrigos-MacBook-Pro.local>
2017-08-27 21:25:45 -0300

    wip aula 4 atividade 6

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,9 +1,46 @@
-const tamanhoFrase = $('.frase').text().split(' ').length;
-$('#numero-palavras').text(tamanhoFrase);
-
+let tempo = $('#tempo-restante').text();
 const campo = $('#campo');
-campo.on('input', () => {
-  const conteudo = campo.val();
-  $('#contador-palavras').text(conteudo.split(/\S+/).length - 1);
-  $('#contador-caracteres').text(conteudo.length);
+
+function atualizaTamanhoFrase() {
+  const frase = $('.frase');
+  const tamanhoFrase = frase.text().split(' ').length;
+  const numeroPalavras = $('#numero-palavras');
+  numeroPalavras.text(tamanhoFrase);
+}
+
+function inicializaContadores() {
+  campo.on('input', () => {
+    const conteudo = campo.val();
+    $('#contador-palavras').text(conteudo.split(/\S+/).length - 1);
+    $('#contador-caracteres').text(conteudo.length);
+  });
+}
+
+function inicializaCronometro() {
+  campo.on('focus', () => {
+    const cronometroID = setInterval(() => {
+      console.log(tempo);
+      $('#tempo-restante').text(tempo -= 1);
+      if (tempo < 1) {
+        campo.attr('disabled', true);
+        clearInterval(cronometroID);
+      }
+    }, 1000);
+  });
+}
+
+function reiniciaJogo() {
+  campo.attr('disabled', false);
+  campo.val('');
+  $('#tempo-restante').text('9');
+  tempo = $('#tempo-restante').text();
+  $('#contador-palavras').text('0');
+  $('#contador-caracteres').text('0');
+}
+
+$(document).ready(() => {
+  atualizaTamanhoFrase();
+  inicializaContadores();
+  inicializaCronometro();
+  $('#botao-reiniciar').on('click', reiniciaJogo);
 });
