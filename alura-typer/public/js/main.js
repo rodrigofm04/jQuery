28346fe5d6f1307380e027ddf08763682373859e
Rodrigo F. Melo <rodrigofm@tst.jus.br>
2017-08-28 16:53:35 -0300

    finalizado curso parte 1

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,14 +1,23 @@
-let tempo = $('#tempo-restante').text();
+const tempoInicial = $('#tempo-digitacao').text();
 const campo = $('#campo');
+var frase = $('.frase').text();
+
+function iniciaMarcadores() {
+  campo.on('input', function() {
+    var digitado = campo.val();
+    var correto = frase.startsWith(digitado);
+    campo.toggleClass('borda-verde', correto);
+    campo.toggleClass('borda-vermelha', !correto);
+  });
+}
 
 function atualizaTamanhoFrase() {
-  const frase = $('.frase');
-  const tamanhoFrase = frase.text().split(' ').length;
+  const tamanhoFrase = frase.split(' ').length;
   const numeroPalavras = $('#numero-palavras');
   numeroPalavras.text(tamanhoFrase);
 }
 
-function inicializaContadores() {
+function iniciaContadores() {
   campo.on('input', () => {
     const conteudo = campo.val();
     $('#contador-palavras').text(conteudo.split(/\S+/).length - 1);
@@ -16,31 +25,46 @@ function inicializaContadores() {
   });
 }
 
-function inicializaCronometro() {
-  campo.on('focus', () => {
-    const cronometroID = setInterval(() => {
-      console.log(tempo);
-      $('#tempo-restante').text(tempo -= 1);
-      if (tempo < 1) {
-        campo.attr('disabled', true);
-        clearInterval(cronometroID);
-      }
+function iniciaCronometro() {
+  
+  campo.on('focus', function() {
+    var tempoRestante = tempoInicial;
+    var cronometroID = setInterval(function() {
+        tempoRestante--;
+        $('#tempo-digitacao').text(tempoRestante);
+        if (tempoRestante < 1) {
+            clearInterval(cronometroID);
+            finalizaJogo();
+        }
     }, 1000);
   });
 }
 
-function reiniciaJogo() {
-  campo.attr('disabled', false);
-  campo.val('');
-  $('#tempo-restante').text('9');
-  tempo = $('#tempo-restante').text();
-  $('#contador-palavras').text('0');
-  $('#contador-caracteres').text('0');
+function finalizaJogo() {
+  campo.attr('disabled', true);
+  campo.toggleClass('campo-desativado');
+  $('#botao-reiniciar').attr('disabled', false);
+  inserePlacar();
 }
 
+ function reiniciaJogo(){
+  $('#botao-reiniciar').attr('disabled', true);
+   campo.attr('disabled',false);
+   campo.val('');
+   $('#contador-palavras').text('0');
+   $('#contador-caracteres').text('0');
+   $('#tempo-digitacao').text(tempoInicial);
+   iniciaCronometro();
+   campo.toggleClass('campo-desativado');
+   campo.removeClass('borda-vermelha');
+   campo.removeClass('borda-verde');
+ }
+
 $(document).ready(() => {
+  $('#botao-reiniciar').attr('disabled', true);
   atualizaTamanhoFrase();
-  inicializaContadores();
-  inicializaCronometro();
+  iniciaContadores();
+  iniciaCronometro();
+  iniciaMarcadores();
   $('#botao-reiniciar').on('click', reiniciaJogo);
 });
---
+++ alura-typer/public/js/placar.js
@@ -0,0 +1,33 @@
+function inserePlacar() {
+    var corpoTabela = $('.placar').find('tbody');
+    console.log(corpoTabela);
+    var usuario = $('#nome-usuario').val();
+    var numPalavras = $('#contador-palavras').text();
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+  
+    corpoTabela.append(linha);
+  }
+  
+  function novaLinha(usuario, numPalavras){
+    var linha = $('<tr>');
+    var colunaUsuario = $('<td>').text(usuario);
+    var colunaPalavras = $('<td>').text(numPalavras);
+    var colunaRemover = $('<td>');
+  
+    var link = $('<a>').attr('href','#').addClass('botao-remover');
+    var icone = $('<i>').addClass('small').addClass('material-icons').text('delete');
+    link.append(icone);
+    colunaRemover.append(link);
+  
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+    
+    return linha;
+  }
+  
+  function removeLinha(event) {
+    event.preventDefault();
+    $(this).parent().parent().remove();
+  }
