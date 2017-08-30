59dd6a0a06bd661402197c4e33bba57881766327
Rodrigo F. Melo <rodrigofm@tst.jus.br>
2017-08-30 16:54:09 -0300

    jquery 2: Aula 3

--- alura-typer/public/js/frase.js
+++ alura-typer/public/js/frase.js
@@ -1,14 +1,23 @@
 $('#botao-frase').click(trocaFrase);
 
 function trocaFrase() {
-    $.get('http://localhost:3000/frases', function (data) {
-        console.log('req para trocar frase');
-        console.log(data);
-        var frase = $('.frase');
-        var i = Math.floor(Math.random() * data.length);
-        console.log(i);
-        frase.text(data[i].texto);
-        atualizaTamanhoFrase();
-        atualizaTempoInicial(data[i].tempo);
+    $('#spinner').show();
+    $.get('http://localhost:3000/frases', trocaFraseAleatoria)
+    .fail(function () {
+        $('#erro').toggle();
+        setTimeout(function () { //toggle o comportamento em 2s
+            $('#erro').toggle();
+        }, 2000);
+    })
+    .always(function () {
+        $('#spinner').toggle();
     });
+}
+
+function trocaFraseAleatoria(data) {
+    var frase = $('.frase');
+    var i = Math.floor(Math.random() * data.length);
+    frase.text(data[i].texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data[i].tempo);
 }
