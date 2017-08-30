d1a011f0931ed5977214b2b92b35463caa999bb8
Rodrigo F. Melo <rodrigofm@tst.jus.br>
2017-08-30 17:22:29 -0300

    jquery 2: Aula 4

--- alura-typer/public/js/frase.js
+++ alura-typer/public/js/frase.js
@@ -1,4 +1,5 @@
 $('#botao-frase').click(trocaFrase);
+$('#botao-frase-id').click(buscaFrase);
 
 function trocaFrase() {
     $('#spinner').show();
@@ -21,3 +22,33 @@ function trocaFraseAleatoria(data) {
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[i].tempo);
 }
+
+function buscaFrase() {
+    $('#spinner').show();
+    var fraseId = $('#frase-id').val();
+    var parametro = {
+        id : fraseId
+    };
+    $.get('http://localhost:3000/frases', parametro, trocaFrase)
+    .fail(function () {
+        $('#erro').toggle();
+        setTimeout(function () { //toggle o comportamento em 2s
+            $('#erro').toggle();
+        }, 2000);
+    })
+    .always(function () {
+        $('#spinner').toggle();
+    });
+
+    console.log('buscando frase');
+}
+
+function trocaFrase(data) {
+    
+        console.log(data);
+    
+        var frase = $(".frase");
+        frase.text(data.texto); //cuidado, texto com "o" no final 
+        atualizaTamanhoFrase();
+        atualizaTempoInicial(data.tempo);
+    }
